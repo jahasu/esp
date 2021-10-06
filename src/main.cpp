381d2d7a0ddc59eff7a6540d7e310730652a6651
Jack Sutcliffe <jack.sutcliffe@fsae.co.nz>
2021-10-07 10:17:24 +1300

    led shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
-#include <FastLED.h>
+#include <Adafruit_NeoPixel.h>
 #include <TMCStepper.h>
 #include <BleKeyboard.h>
-#include "FastLED_RGBW.h"
+#include <math.h>
+
 
 
 //tmc stepper 
@@ -19,39 +20,28 @@
 TMC2130Stepper driver(CS_PIN, R_SENSE, SW_MOSI, SW_MISO, SW_SCK);
 
 //button pin setup
-#define buttonR 18
-#define redCH 0
-#define buttonG 19
+#define buttonR 33
+#define redCH 3
+#define buttonG 25
 #define greenCH 1
-#define buttonB 21
+#define buttonB 26
 #define blueCH 2
-#define buttonInput 2
+#define buttonInput 14
 
 #define freq 5000
 #define resolution 8
 
 bool buttonPressed = false;
 //neopixel setup
-#define NUM_LEDS 100
-#define DATA_PIN 6
+#define NUMPIXELS 160
+#define PIN        27
+
+#define MATH_MIN_B 0
+#define MATH_MAX_B 255
+#define MATH_SPREAD 0.1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRBW + NEO_KHZ800);
 //Bluetooth Control Setup
 BleKeyboard bleKeyboard;
-
-
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
-int buttonBrightnessR = 0;
-int fadeAmount = 2;
-
-static uint8_t hue = 180;
-CRGBW leds[NUM_LEDS];
-CRGB *ledsRGB = (CRGB *) &leds[0];
-uint8_t abrightness = 0;
-const uint8_t brightness = 255;
 
 
 //stepper setup
@@ -60,42 +50,6 @@ const uint8_t brightness = 255;
 
 
 
-
-
-
-void setup() {
-//serial
-Serial.begin(115200); 
-
-//LED strip
-FastLED.addLeds<WS2812B, DATA_PIN, RGB>(ledsRGB, getRGBWsize(NUM_LEDS));
-FastLED.setBrightness(brightness);
-FastLED.show();
-
-//button and LEDs
-pinMode(buttonPin,INPUT_PULLUP);
-
-ledcSetup(redCH, freq, resolution);
-ledcSetup(blueCH, freq, resolution);
-ledcSetup(greenCH, freq, resolution);
-ledcAttachPin(buttonR,redCH);
-ledcAttachPin(buttonG,greenCH);
-ledcAttachPin(buttonB,blueCH); 
-
-int pulse_i =0 ;
-bool pulse_dir = 0;
-
-
-
-//steppers
-
-//bluetooth
-bleKeyboard.begin();
-
-
-runmain();
-}
-
 void setRGB(int colour){
   ledcWrite(redCH, colour);
   ledcWrite(greenCH, colour);
@@ -103,23 +57,55 @@ void setRGB(int colour){
 }
   
 void buttonPulse(){
-    while(digitalRead(buttonInput)== 1){
-    setRGB(i);
-    if(pulse_i == 255 || pulse_i == 0){
+    
+    int pulse_dir = 0;
+    int pulse_i = 0;
+    
+    
+    while(!digitalRead(buttonInput)){
+    setRGB(pulse_i);
+    if(pulse_i == 200 || pulse_i == 0){
       pulse_dir =!pulse_dir;
     }
     if (pulse_dir == 0){
-      i--;
+      pulse_i--;
     }
     else{
-      i++;
+      pulse_i++;
     }
-    delay(20);
+    delay(5);
+
+ 
+    
 
   }
+
 }
 void lightWave(){
+   
+   pixels.clear();
 
+  Serial.println("light wave");
+  int red_arr[NUMPIXELS*2];
+  int blue_arr[NUMPIXELS*2];
+  for(int i = 0; i< NUMPIXELS*2;i++){//gnerete array
+  
+  double b_x = i*0.09;
+
+ double r_x = i*0.14;
+  blue_arr[i]= ((MATH_MAX_B - MATH_MIN_B)* pow(sin(b_x),2.0))+MATH_MIN_B;
+  red_arr[i]= ((MATH_MAX_B - MATH_MIN_B)* pow(sin(r_x),2.0))+MATH_MIN_B;
+
+  }
+  for(int i = 0; i<NUMPIXELS; i++){ //show array
+    pixels.setPixelColor(i, red_arr[i], 0, blue_arr[i],0);
+  }
+  
+  pixels.show();
+  Serial.println("show array");
+  delay(2000);
+
+//move array
 }
 int lidMove(int lidDir){
   return 1;
@@ -134,16 +120,25 @@ int iPadMove(int ipadDir){
 
 
 void runmain(){
+  /*setRGB(0);
+  delay(1000);
+    
+  buttonPulse();
  
+  ledcWrite(redCH, 255);
+
   
-  
-  buttonPulse():
-  
+  bleKeyboard.write(KEY_RETURN);
+  delay(500);
+  bleKeyboard.write(KEY_RETURN);
+  delay(1000);
+  bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+  */
   lightWave();
   lidMove(1); // 1 is open, 0 is closed  returns 1 if successful - will may need error catching LIMIT SWITCH?
   iPadMove(1);
   delay(1000);
-  ipadMove(0);
+  iPadMove(0);
   lidMove(0);
 
 
@@ -153,7 +148,57 @@ void runmain(){
 
 
 
+
+
+
+
+
+
+
+void setup() {
+
+
+//serial
+Serial.begin(115200); 
+
+
+//button and LEDs
+pinMode(buttonInput,INPUT);
+
+ledcSetup(redCH, freq, resolution);
+ledcSetup(blueCH, freq, resolution);
+ledcSetup(greenCH, freq, resolution);
+ledcAttachPin(buttonR,redCH);
+ledcAttachPin(buttonG,greenCH);
+ledcAttachPin(buttonB,blueCH); 
+
+
+
+//steppers
+
+//bluetooth
+bleKeyboard.begin();
+
+//neopixels
+pixels.begin();
+pixels.show();
+/*
+while(!bleKeyboard.isConnected()) {
+  ledcWrite(redCH, 255);
+delay(100);
+ledcWrite(redCH, 0);
+delay(100);
+}
+*/
+runmain();
+}
+
+
+
+
 void loop() {
+  
+
   //nothing to put here?
 }
 
